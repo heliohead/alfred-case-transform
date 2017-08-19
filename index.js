82090f02c250558e3c31d2fae101f88180f19f25
Helio Oliveira <hheelliioo@live.com>
2017-08-19 14:42:32 -0300

    Add camelCase

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 const alfy = require('alfy');
+const camelCase = require('camelcase');
 
 alfy.output([
   {
-    title: 'Unicorn',
-    subtitle: alfy.input
+    title: camelCase(alfy.input),
+    arg: camelCase(alfy.input),
+    subtitle: 'camelCase',
+    icon: { path: 'icons/camelCase.png' }
   }
 ]);
